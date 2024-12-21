af50ae10f04b8a4723b4e9fed709e80d51dbb31b
jonata.silva <jonata.silva@izatta.tech>
2024-12-21 16:14:40 -0300

    Inicializando projeto

---
+++ src/test/java/com/project/spring/rest_with_springboot/RestWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.spring.rest_with_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
